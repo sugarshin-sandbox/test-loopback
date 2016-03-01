44a24de686317f6658f8c12b672d21dcff6329b6
sato <sato@tsumikiinc.com>
2016-03-01 12:10:56 +0900

    first commit

---
+++ client/index.js
@@ -0,0 +1,30 @@
+// // create user
+// fetch('/api/Users', {
+//   method: 'post',
+//   headers: {
+//     Accept: 'application/json',
+//     'Content-Type': 'application/json'
+//   },
+//   body: JSON.stringify({
+//     username: 'aoi',
+//     email: 'shinsugar@gmail.com',
+//     password: 'mikisato'
+//   })
+// })
+//   .then(res => res.json())
+//   .then(json => console.log(json))
+
+// // login
+// fetch('/api/Users/login', {
+//   method: 'post',
+//   headers: {
+//     Accept: 'application/json',
+//     'Content-Type': 'application/json'
+//   },
+//   body: JSON.stringify({
+//     email: 'shinsugar@gmail.com',
+//     password: 'mikisato'
+//   })
+// })
+//   .then(res => res.json())
+//   .then(json => console.log(json))
